880d8d9cd7d9ad0de2c78b8b209ffc8d86036137
Joel Flores <j_flores88@outlook.com>
2022-05-26 20:49:42 -0700

    Finish tests for capitalize function

--- scripts/capitalize.js
+++ scripts/capitalize.js
@@ -1,5 +1,6 @@
 function capitalize(str) {
-    return str[0].toUpperCase() + str.substring(1, str.length);
+    if (typeof(str) !== "string" || str.length === 0) return 'Please enter valid string.';
+    return str[0].toUpperCase() + str.substring(1, str.length).toLowerCase();
 };
 
 module.exports = capitalize;
--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,1 +1,20 @@
-import capitalize from "../scripts/capitalize";
+const capitalize = require('../scripts/capitalize');
+
+test('Checks input is string.', () => {
+    expect(capitalize(123)).toBe('Please enter valid string.');
+    expect(capitalize({ 0: 0 })).toBe('Please enter valid string.');
+    expect(capitalize([1, 2, 3])).toBe('Please enter valid string.');
+})
+
+test('Checks for valid string length.', () => {
+    expect(capitalize('')).toBe('Please enter valid string.');
+});
+
+test('Capitalizes the first letter of a string.', () => {
+    expect(capitalize('first test.')).toBe('First test.');
+});
+
+test('Fixes Camelcase & Uppercase strings.', () => {
+    expect(capitalize('CaMeLcAsE tEst.')).toBe('Camelcase test.');
+    expect(capitalize('UPPERCASE TEST.')).toBe('Uppercase test.')
+});
