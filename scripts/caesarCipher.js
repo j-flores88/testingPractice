e1b65e388d268418ca4fa305f365cbdf20b1acc1
Joel Flores <j_flores88@outlook.com>
2022-06-02 17:07:47 -0700

    Remove test that doesnt accept negative numbers

--- scripts/caesarCipher.js
+++ scripts/caesarCipher.js
@@ -1,5 +1,7 @@
 function caesarCipher(str, shift) {
+    const alpha = 'abcdefghijklmnopqrstuvwxyz'.split('');
     let newStr = '';
+
     if (typeof(str) !== 'string') return 'Please enter a valid string.'
     if (shift < 0 || !Number(shift)) return 'Please enter a valid number.'
 
--- tests/caesarCipher.test.js
+++ tests/caesarCipher.test.js
@@ -4,10 +4,6 @@ test('It checks if a string is passed in.', () => {
     expect(caesarCipher(123, 1)).toBe('Please enter a valid string.');
 });
 
-test('It does not accept a negative number for an argument', () => {
-    expect(caesarCipher('string', -1)).toBe('Please enter a valid number.');
-});
-
 test('It checks if shift is a number', () => {
     expect(caesarCipher('string', 'g')).toBe('Please enter a valid number.');
 });
